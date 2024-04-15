9f3180a47352db06bae6c5e31a8023d50b54cc92
swparabellum <swparabellum@gmail.com>
2024-04-15 16:56:17 +0900

    연습 웹사이트 제작.

---
+++ 웹만들기/script/script.js
@@ -0,0 +1,8 @@
+function clickBtn(color){
+    if(color == yellow){
+        document.body.style.backgroundColor = "yellow";
+    }else if(color == white){
+        document.body.style.backgroundColor = "white";
+    }
+    
+}
